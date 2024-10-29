2d91a3cc5260284a86d2178701f94fd54d5150f8
codie1982 <engin_erol@hotmail.com>
2024-10-29 21:47:08 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+require("dotenv").config()
+const path = require('path');
+const compression = require('compression');
+const express = require("express")
+const session = require('express-session');
+const colors = require("colors")
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { createGzip } = require('zlib');
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 3000;
+
+const app = express()
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); // Form-data verisini almak için
+app.use(express.static(path.join(__dirname, 'public')));
+// GZIP sıkıştırmasını etkinleştir
+app.use(compression());
+// Middleware
+app.use(fileUpload({
+  //limits: { fileSize: 5 * 1024 * 1024 * 1024 }, // 5 GB aws sunucusunun bir kerede max upload miktarı.
+  limits: { fileSize: 10 * 1024 * 1024 }, // 5 GB aws sunucusunun bir kerede max upload miktarı.
+}));
+app.get("/", (req, res) => {
+    res.json({
+        data:"Hello world"
+    })
+  })
+
+app.listen(PORT, () => { console.log(`Started on Port : ${PORT}`) })
