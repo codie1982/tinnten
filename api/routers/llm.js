13621f104708bbe0cdcfd6b8682b8596ffcee3a4
codie1982 <engin_erol@hotmail.com>
2024-11-01 23:56:19 +0300

    adding llm intergate

--- api/routers/llm.js
+++ api/routers/llm.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const route = express.Router()
-const { searchProduct } = require("../controller/searchProduct")
+const { searchProduct } = require("../controller/product")
+const { addCompanyVector } = require("../controller/company")
 route.post("/search", searchProduct)
+route.post("/vector", addCompanyVector)
 module.exports = route
